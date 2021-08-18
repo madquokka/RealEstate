9a2e1cef9598c6e2486ab010f110392f19d78577
Kyle Todd <ktodd087@gmail.com>
2021-08-18 11:10:21 +1000

    Setup client side react

--- server/server.js
+++ server/server.js
@@ -5,6 +5,16 @@ const PORT = 8080;
 //  DB Configuration
 require('./src/database');
 
+const CLIENT_BUILD_PATH = path.join(__dirname, "../client/build");
+
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
+
+// Server React Client
+app.get("/", function (req, res) {
+    res.sendFile(path.join(CLIENT_BUILD_PATH, "index.html"));
+});
+
 app.get('/', (req, res) => {
     res.send("Hello World ! ");
 });
@@ -12,3 +22,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, function () {
     console.log(`Server Listening on ${PORT}`);
 });
+
+// will redirect all the non-api routes to react frontend
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+});
+
